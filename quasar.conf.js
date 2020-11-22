08291f0ece48ad3d04e1e6d45496d7dcf8ef9691
Justin Dunsworth <justin.dunsworth@gmail.com>
2020-11-22 16:58:38 -0600

    Initial quasar and electron configuration

--- quasar.conf.js
+++ quasar.conf.js
@@ -1,13 +1,11 @@
-/*
- * This file runs in a Node context (it's NOT transpiled by Babel), so use only
- * the ES6 features that are supported by your Node version. https://node.green/
- */
-
-// Configuration for your app
+// Configuration for Parley
 // https://quasar.dev/quasar-cli/quasar-conf-js
 /* eslint-env node */
 /* eslint func-names: 0 */
 /* eslint global-require: 0 */
+
+const path = require('path');
+const webpack = require('webpack');
 
 module.exports = function (/* ctx */) {
   return {
@@ -22,6 +20,7 @@ module.exports = function (/* ctx */) {
     // https://quasar.dev/quasar-cli/boot-files
     boot: [
       'axios',
+      'parley',
     ],
 
     // https://quasar.dev/quasar-cli/quasar-conf-js#Property%3A-css
@@ -43,7 +42,7 @@ module.exports = function (/* ctx */) {
       'material-icons', // optional, you are not bound to it
     ],
 
-    // Full list of options: https://quasar.dev/quasar-cli/quasar-conf-js#Property%3A-build
+    // https://quasar.dev/quasar-cli/quasar-conf-js#Property%3A-build
     build: {
       vueRouterMode: 'hash', // available values: 'hash', 'history'
 
@@ -71,36 +70,50 @@ module.exports = function (/* ctx */) {
           loader: 'eslint-loader',
           exclude: /node_modules/,
         });
+
+        cfg.resolve.alias = {
+          ...cfg.resolve.alias,
+          '~': path.resolve(__dirname, './'),
+          '@': path.resolve(__dirname, './src'),
+          '@layouts': path.resolve(__dirname, './src/layouts'),
+          '@pages': path.resolve(__dirname, './src/pages'),
+          '@components': path.resolve(__dirname, './src/components'),
+          '@store': path.resolve(__dirname, './src/store'),
+          '@router': path.resolve(__dirname, './src/router'),
+          '@boot': path.resolve(__dirname, './src/boot'),
+          '@statics': path.resolve(__dirname, './src/statics'),
+          '@css': path.resolve(__dirname, './src/css'),
+          '@assets': path.resolve(__dirname, './src/assets'),
+          '@mixins': path.resolve(__dirname, './src/mixins'),
+        };
+
+        cfg.plugins.push(new webpack.DefinePlugin({
+          'process.env': {
+            PACKAGE_JSON: `"${escape(JSON.stringify(require('./package.json')))}"`,
+          },
+        }));
       },
     },
 
-    // Full list of options: https://quasar.dev/quasar-cli/quasar-conf-js#Property%3A-devServer
+    // https://quasar.dev/quasar-cli/quasar-conf-js#Property%3A-devServer
     devServer: {
       https: false,
       port: 8080,
-      open: true, // opens browser window automatically
+      open: true,
     },
 
     // https://quasar.dev/quasar-cli/quasar-conf-js#Property%3A-framework
     framework: {
-      iconSet: 'material-icons', // Quasar icon set
-      lang: 'en-us', // Quasar language pack
+      iconSet: 'fontawesome',
+      lang: 'en-us',
       config: {},
-
-      // Possible values for "importStrategy":
-      // * 'auto' - (DEFAULT) Auto-import needed Quasar components & directives
-      // * 'all'  - Manually specify what to import
       importStrategy: 'auto',
-
-      // For special cases outside of where "auto" importStrategy can have an impact
-      // (like functional components as one of the examples),
-      // you can manually specify Quasar components/directives to be available everywhere:
-      //
       // components: [],
       // directives: [],
-
-      // Quasar plugins
-      plugins: [],
+      plugins: [
+        'Notify',
+        'Meta',
+      ],
     },
 
     // animations: 'all', // --- includes all animations
@@ -154,19 +167,19 @@ module.exports = function (/* ctx */) {
       },
     },
 
-    // Full list of options: https://quasar.dev/quasar-cli/developing-cordova-apps/configuring-cordova
+    // https://quasar.dev/quasar-cli/developing-cordova-apps/configuring-cordova
     cordova: {
       // noIosLegacyBuildFlag: true, // uncomment only if you know what you are doing
     },
 
-    // Full list of options: https://quasar.dev/quasar-cli/developing-capacitor-apps/configuring-capacitor
+    // https://quasar.dev/quasar-cli/developing-capacitor-apps/configuring-capacitor
     capacitor: {
       hideSplashscreen: true,
     },
 
-    // Full list of options: https://quasar.dev/quasar-cli/developing-electron-apps/configuring-electron
+    // https://quasar.dev/quasar-cli/developing-electron-apps/configuring-electron
     electron: {
-      bundler: 'packager', // 'packager' or 'builder'
+      bundler: 'builder',
 
       packager: {
         // https://github.com/electron-userland/electron-packager/blob/master/docs/api.md#options
@@ -184,7 +197,7 @@ module.exports = function (/* ctx */) {
       builder: {
         // https://www.electron.build/configuration/configuration
 
-        appId: 'parley',
+        appId: 'parley-for-canvas',
       },
 
       // More info: https://quasar.dev/quasar-cli/developing-electron-apps/node-integration
--- src-electron/main-process/electron-main.js
+++ src-electron/main-process/electron-main.js
@@ -1,29 +1,36 @@
-import { app, BrowserWindow, nativeTheme } from 'electron'
+import { app, BrowserWindow, nativeTheme } from 'electron';
+import fs from 'fs';
 
 try {
   if (process.platform === 'win32' && nativeTheme.shouldUseDarkColors === true) {
-    require('fs').unlinkSync(require('path').join(app.getPath('userData'), 'DevTools Extensions'))
+    require('fs').unlinkSync(require('path').join(app.getPath('userData'), 'DevTools Extensions'));
   }
-} catch (_) { }
-
-/**
- * Set `__statics` path to static files in production;
- * The reason we are setting it here is that the path needs to be evaluated at runtime
- */
-if (process.env.PROD) {
-  global.__statics = __dirname
+} catch (_) {
+  //
 }
 
-let mainWindow
-
-function createWindow () {
-  /**
-   * Initial window options
+/**
+   * Set `__statics` path to static files in production;
+   * The reason we are setting it here is that the path needs to be evaluated at runtime
    */
+if (process.env.PROD) {
+  global.__statics = __dirname;
+}
+
+let mainWindow;
+
+function createWindow() {
+  /**
+     * Initial window options
+     */
   mainWindow = new BrowserWindow({
     width: 1000,
     height: 600,
+    backgroundColor: '#222',
+    title: 'Parley for Canvas',
+    darkTheme: true,
     useContentSize: true,
+    autoHideMenuBar: true,
     webPreferences: {
       // Change from /quasar.conf.js > electron > nodeIntegration;
       // More info: https://quasar.dev/quasar-cli/developing-electron-apps/node-integration
@@ -32,26 +39,48 @@ function createWindow () {
 
       // More info: /quasar-cli/developing-electron-apps/electron-preload-script
       // preload: path.resolve(__dirname, 'electron-preload.js')
-    }
-  })
+    },
+  });
 
-  mainWindow.loadURL(process.env.APP_URL)
+  // Get User Configuration Directory
+  const configPath = `${app.getPath('userData')}\\parley-config.json`;
+
+  // Set our initial route to load with the selector
+  // eslint-disable-next-line no-unused-vars
+  let routeToLoad = process.env.APP_URL;
+
+  // Check if configuration file exists
+  if (fs.existsSync(configPath)) {
+    // Load configuration file
+    try {
+      const loadedConfigFile = fs.readFileSync(configPath, 'utf-8');
+      const configFileData = JSON.parse(loadedConfigFile);
+
+      if (configFileData.url) {
+        routeToLoad = `${process.env.APP_URL}#${configFileData.url}`;
+      }
+    } catch (err) {
+      console.log('Unable to load configuration file. Falling back to selector...');
+    }
+  }
+
+  mainWindow.loadURL(process.env.APP_URL);
 
   mainWindow.on('closed', () => {
-    mainWindow = null
-  })
+    mainWindow = null;
+  });
 }
 
-app.on('ready', createWindow)
+app.on('ready', createWindow);
 
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
-    app.quit()
+    app.quit();
   }
-})
+});
 
 app.on('activate', () => {
   if (mainWindow === null) {
-    createWindow()
+    createWindow();
   }
-})
+});
