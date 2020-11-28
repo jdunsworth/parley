f4670d41ffdc14e4f82193686408afcff74db9d9
cjpitch23 <chad.pitchford@potawatomi.org>
2020-11-28 01:24:35 -0600

    Worked on Add Profile layout
    Added card with input to add profiles.
    Added help button to walk user through finding their ApiKey.
    Added button link to about page for users looking into the application.
    TODO store entered value in local storage

--- src/pages/profiles/Add.vue
+++ src/pages/profiles/Add.vue
@@ -1,13 +1,90 @@
 <template>
   <q-page padding>
-    Add Profile
+    <!-- Add Profile Key -->
+    <q-card dark bordered class="bg-grey-9 absolute absolute-center add-profile-card">
+      <q-card-section>
+        <div class="text-h6">
+          <div class="row justify-between">
+            <div class="col-8">Add Profile</div>
+            <div class="col-1">
+              <q-icon
+                name="fas fa-question-circle"
+                class="cursor-pointer"
+                @click="help = !help"
+              >
+                <q-tooltip anchor="top middle" self="bottom middle" class="bg-info">
+                  Click for help!
+                </q-tooltip>
+              </q-icon>
+            </div>
+          </div>
+        </div>
+      </q-card-section>
+
+      <q-separator dark inset />
+
+      <!-- Help Section -->
+      <q-card-section v-if="help">
+        <p>In order to add a profile from Canvas, we need you to enter
+        the api key assigned to your account here.</p>
+
+        <p>You can find that api key by following these steps:</p>
+        <ol class="text-weight-light">
+          <li>Log into the Canvas account you want to add.</li>
+          <li>Click on the <strong>Account</strong> tab in the top right.</li>
+          <li>Click on <strong>Settings</strong>.</li>
+          <li>
+            Underneath <strong>Approved Integrations</strong>,
+            click on the <strong>New Access Token</strong> button.
+          </li>
+          <li>Enter <strong>Parley</strong> as the <strong>Purpose</strong>.</li>
+          <li>Leave <strong>Expires</strong> blank.</li>
+          <li>Click <strong>Generate Token</strong>.</li>
+          <li>Copy <strong>Token</strong> and paste it down below!</li>
+        </ol>
+
+        <p>
+          <strong>Warning!</strong>
+          <em>
+            Once you leave that page on Canvas you won't be able to retrieve the
+            full token anymore, you'll have to regenerate it to
+            get a new value.
+          </em>
+        </p>
+      </q-card-section>
+
+      <!-- API Key Form -->
+      <q-card-section>
+        <q-input v-model="apiKey" label="Profile Key" filled />
+      </q-card-section>
+
+      <q-card-actions align="between">
+        <q-btn label="About" color="info" to="/about" />
+        <q-btn label="Add" color="positive" :disable="!apiKey" @click="addApiKey" />
+      </q-card-actions>
+    </q-card>
   </q-page>
 </template>
 
 <script>
 export default {
   name: 'ProfileAdd',
+  data() {
+    return {
+      apiKey: null,
+      help: false,
+    };
+  },
   methods: {
+    addApiKey() {
+      // TODO :: Store Api Key
+      // TODO :: Redirect to profile overview
+    },
   },
 };
 </script>
+
+<style lang="stylus">
+.add-profile-card
+  width 500px
+</style>
